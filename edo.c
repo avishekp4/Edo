85b25d8d0530dc7787134d132b543e7f18546ca1
Xnax <avishekp4@gmail.com>
2019-09-10 01:55:35 +0530

    Turn off echoing and Disable raw mode at exit

--- edo.c
+++ edo.c
@@ -1,8 +1,29 @@
 #include<stdio.h>
+
+#include <stdlib.h>
+#include <termios.h>
 #include <unistd.h>
 
-int main()
-{
+struct termios orig_termios_setting;
+
+void diableRawMode() {
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &orig_termios_setting);
+}
+
+void enableRawMode() {
+    atexit(diableRawMode);
+    struct termios raw;
+
+    tcgetattr(STDIN_FILENO, &raw);
+
+    orig_termios_setting = raw;
+    raw.c_lflag &= ~(ECHO);
+
+    tcsetattr(STDIN_FILENO, TCSAFLUSH, &raw);
+}
+
+int main() {
+    enableRawMode();
     char c;
     while (read(STDIN_FILENO, &c, 1) == 1 && c != 'q');
     return 0;
