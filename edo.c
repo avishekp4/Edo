f3841cc50c133af0750d735c6fd20b418cf8a0e1
Xnax <avishekp4@gmail.com>
2019-09-16 03:05:49 +0530

    Preocess read seperately

--- edo.c
+++ edo.c
@@ -46,24 +46,47 @@ void enableRawMode() {
         killP("tcsetattr");
     }
 }
-
+char readKeyPress(){
+    int nread;
+    char c;
+    while( (nread = read(STDIN_FILENO, &c, 1)) != 1 ) {
+        if(nread == -1 & errno != EAGAIN){
+            killP("Read");
+        }
+    }
+    return c;
+}
+/*Input*/
+void processKeyPress() {
+    char c = readKeyPress();
+    switch (c)
+    {
+    case (CTRL_KEY('q')):
+        exit(0);
+        break;
+    
+    default:
+        break;
+    }
+ }
 /*Intit section*/
 
 int main() {
     enableRawMode();
 
     while (1) {
-        char c = '\0';
-        if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) killP("Read");
-        if(iscntrl(c)){
-            printf("%d \r\n",c);
-        }
-        else {
-            printf("%d ('%c') \r\n",c ,c);
-        }
-        if(c == CTRL_KEY('q')) {
-            break;
-        }
+        // char c = '\0';
+        // if(read(STDIN_FILENO, &c, 1) == -1 && errno != EAGAIN) killP("Read");
+        // if(iscntrl(c)){
+        //     printf("%d \r\n",c);
+        // }
+        // else {
+        //     printf("%d ('%c') \r\n",c ,c);
+        // }
+        // if(c == CTRL_KEY('q')) {
+        //     break;
+        // }
+        processKeyPress();
     }
     return 0;
 }
