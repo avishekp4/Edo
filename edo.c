a5531f4440f1cf27f459eb0302cb19c562d7aa4d
Xnax <avishekp4@gmail.com>
2019-09-17 15:22:58 +0530

    Remove 1|| placed for testing

--- edo.c
+++ edo.c
@@ -74,32 +74,16 @@ int getCursorPosition(int *rows,int *cols) {
         i++;
     }
     buff[i] ='\0';
-
-    printf("\r\n&buff[1]: '%s'\r\n", &buff[1]);
-    
     if(buff[0] != '\x1b' || buff[1] != '[') return -1;
     if(sscanf(&buff[2], "%d;%d", rows, cols) != 2) return -1;
-    // printf("\r\n");
-    // char c;
-    // while(read(STDIN_FILENO,&c,1) == 1) {
-    //     if(iscntrl(c)) {
-    //         printf("%d\r\n", c);
-    //     } else {
-    //         printf("%d ('%c')\r\n",c,c);
-    //     }
-    // }
-   // readKeyPress();
-
-    //return -1;
     return 0;
 }
 
 int getWindowSize(int *rows, int *cols) {
     struct winsize ws;
-    if(1 || ioctl(STDOUT_FILENO,TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0){
+    if( ioctl(STDOUT_FILENO,TIOCGWINSZ, &ws) == -1 || ws.ws_col == 0) {
         if(write(STDOUT_FILENO,"\x1b[999C\x1b[999B",12) != 12) return -1;
-         return getCursorPosition(rows,cols);//readKeyPress();
-        return -1;
+        return getCursorPosition(rows,cols);
     } else {
         *cols = ws.ws_col;
         *rows = ws.ws_row;
